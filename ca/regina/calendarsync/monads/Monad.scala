346d632a30ac265ddf40697294dfe0e643f97f6c
Andrijan Jelovic <ajelovic@yahoo.com>
2015-03-12 15:55:15 -0600

    Moved stuff around

--- ca/regina/calendarsync/monads/Monad.scala
+++
@@ -1,7 +0,0 @@
-package ca.regina.calendarsync.monads
-
-trait Monad[F[_]] {
-	def pure[A](a: A): F[A]
-	def map[A, B](fa: F[A])(f: A => B): F[B]
-	def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
-}
--- ca/regina/calendarsync/syncadapter/Deletions.scala
+++
@@ -1,8 +0,0 @@
-package ca.regina.calendarsync.syncadapter
-
-import android.content.Context
-import ca.regina.calendarsync.monads.IO
-
-class Deletions(context: Context) {
-	def deleteBasedOnUrl(url: android.net.Uri): IO[Int] = IO(context.getContentResolver().delete(url, null, null))
-}
